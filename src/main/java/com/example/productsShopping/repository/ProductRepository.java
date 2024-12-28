f645fb96bbb23dba9a40bbdb18c5d3b75b68012b
N1jat0r <nijat85piriyev85@gmail.com>
2024-12-28 12:35:43 +0400

    first commit

---
+++ src/main/java/com/example/productsShopping/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.example.productsShopping.repository;
+
+import com.example.productsShopping.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByIdAndUser_Username(Long productId, String username);
+}
---
+++ src/main/java/com/example/productsShopping/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.example.productsShopping.security;
+
+import com.example.productsShopping.entity.User;
+import com.example.productsShopping.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username: " + username)
+                );
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities("USER")
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+}
